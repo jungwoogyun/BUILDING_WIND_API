4ade340071b0126cc3059c157cfa5139d86b0405
jungwoogyun <jwg8910@naver.com>
2023-12-14 10:21:38 +0900

    .

--- UI/js/common.js
+++ UI/js/common.js
@@ -83,3 +83,45 @@ e.preventDefault();
 //--------------------------
 // MAP CODE
 //--------------------------
+	// Leaflet 초기화
+	var map = L.map('map').setView([37.5729, 126.9794], 15);
+
+  L.tileLayer('https://tiles.osm.kr/hot/{z}/{x}/{y}.png', {
+		maxZoom: 19,
+		attribution: '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap</a>'
+	}).addTo(map);
+
+  	// 다중 마커를 추가하고 각 마커에 팝업 설정
+	var markers = [
+		{latlng: [37.5729, 126.9794], popupContent: '서울특별시 종로구'},
+		{latlng: [37.5631, 126.9829], popupContent: '경복궁'}
+	];
+  
+	markers.forEach(function(markerInfo) {
+		var marker = L.marker(markerInfo.latlng).addTo(map);
+		marker.bindPopup(markerInfo.popupContent);
+	});
+
+  	// 클릭한 위치의 정보 업데이트 함수
+	function updateInfoPanel(latlng) {
+		// 클릭한 위치의 위도와 경도 업데이트
+		document.getElementById('latitude').textContent = latlng.lat.toFixed(6);
+		document.getElementById('longitude').textContent = latlng.lng.toFixed(6);
+
+		// 클릭한 위치의 주소를 지오코딩을 통해 가져오기
+		fetch('https://api.opencagedata.com/geocode/v1/json?q=' + latlng.lat + '+' + latlng.lng + '&key=YOUR_OPENCAGE_API_KEY')
+			.then(response => response.json())
+			.then(data => {
+				const address = data.results[0].formatted;
+				document.getElementById('address').textContent = address;
+			})
+			.catch(error => {
+				console.error('Error fetching address:', error);
+			});
+	}
+
+	// 지도 클릭 이벤트 리스너
+	map.on('click', function (e) {
+		// 클릭한 지점의 정보를 사용하여 정보 패널 내용을 업데이트
+		updateInfoPanel(e.latlng);
+	});
