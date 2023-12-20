b73d9b84c2b7b7d8afdf17559e1c55f17362bcd7
jungwoogyun <jwg8910@naver.com>
2023-12-21 03:15:57 +0900

    .

---
+++ BUILDING_WIND_API_REQ_SERVER/src/main/java/com/example/demo/properties/RealTimeProperties.java
@@ -0,0 +1,7 @@
+package com.example.demo.properties;
+
+public class RealTimeProperties {
+    //부산광역시 해운대구 nx,ny
+    public static final String nx = "99";
+    public static final String ny = "75";
+}
